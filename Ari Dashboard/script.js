550469475d3fdf31b8e56500fe10bdd65ff4da31
Greghi2000 <greghi.fumagalli@gmail.com>
2023-02-26 17:53:37 +0000

    fixed hamburger menu

--- Ari Dashboard/script.js
+++ Ari Dashboard/script.js
@@ -1,12 +1,21 @@
-function searchCity() {
-    console.log('HII')
-}
+// function searchCity() {
+//     console.log('HII')
+// }
 
-function httpGet(theUrl){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+// function httpGet(theUrl){
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
 
 // the url I have to put first my backend IP address and then the route, e.g "/" or "/ProductId"
+
+function myFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
