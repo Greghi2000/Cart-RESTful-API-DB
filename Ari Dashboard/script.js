f11a1e6fbdbf34162712a46ea019a1c09a9922be
Greghi2000 <greghi.fumagalli@gmail.com>
2023-03-11 11:33:10 +0000

    Cleaned up code infrastructure

--- Ari Dashboard/script.js
+++ Ari Dashboard/script.js
@@ -1,16 +1,3 @@
-// function searchCity() {
-//     console.log('HII')
-// }
-
-// function httpGet(theUrl){
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-//     xmlHttp.send( null );
-//     return xmlHttp.responseText;
-// }
-
-// the url I have to put first my backend IP address and then the route, e.g "/" or "/ProductId"
-
 function myFunction() {
   let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
